414730492a20bdf4191defa2ad2837182c5e16d9
Victor Hugo <victorhugoos@live.com>
2023-01-17 19:14:42 -0300

    feat: added '/authenticate' route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/use-cases/create-client/CreateClientController";
+import { AuthenticateClientController } from "./modules/accounts/use-cases/authenticate-client/AuthenticateClientController";
 
 const routes = Router();
 
 routes.post("/client", new CreateClientController().handle);
+
+routes.post("/authenticate", new AuthenticateClientController().handle);
 
 export { routes };
