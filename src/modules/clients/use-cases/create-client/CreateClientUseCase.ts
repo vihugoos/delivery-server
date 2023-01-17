dfc8ab5cd64fbdfcf3142a826eec7f5607acafc2
Victor Hugo <victorhugoos@live.com>
2023-01-17 17:49:39 -0300

    feat: CreateClientUseCase.ts created

---
+++ src/modules/clients/use-cases/create-client/CreateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../../database/prisma-client";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+    username: string;
+    password: string;
+}
+
+class CreateClientUseCase {
+    async execute({ username, password }: ICreateClient) {
+        const clientExists = await prisma.clients.findFirst({
+            where: {
+                username: {
+                    equals: username,
+                    mode: "insensitive",
+                },
+            },
+        });
+
+        if (clientExists) {
+            throw new Error("Client already exists.");
+        }
+
+        const hashPassword = await hash(password, 10);
+
+        const client = await prisma.clients.create({
+            data: {
+                username,
+                password: hashPassword,
+            },
+        });
+
+        return client;
+    }
+}
+
+export { CreateClientUseCase };
