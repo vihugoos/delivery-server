11d534abe04f74678a6fc5ac76324702402e8ab7
Victor Hugo <victorhugoos@live.com>
2023-01-18 19:31:33 -0300

    feat: FindDeliveriesByClientController.ts created

---
+++ src/modules/clients/use-cases/find-deliveries-by-client/FindDeliveriesByClientController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { FindDeliveriesByClientUseCase } from "./FindDeliveriesByClientUseCase";
+
+class FindDeliveriesByClientController {
+    async handle(request: Request, response: Response) {
+        const { id_client } = request;
+
+        const findDeliveriesByClientUseCase =
+            new FindDeliveriesByClientUseCase();
+
+        const deliveries = await findDeliveriesByClientUseCase.execute(
+            id_client
+        );
+
+        return response.json({ deliveries });
+    }
+}
+
+export { FindDeliveriesByClientController };
