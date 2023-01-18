b5bc2c6345a7ca05305c0034aff40b8ab5a8b42f
Victor Hugo <victorhugoos@live.com>
2023-01-18 20:04:41 -0300

    feat: FindDeliveriesByDeliverymanController.ts created

---
+++ src/modules/deliveryman/use-cases/find-deliveries-by-deliveryman/FindDeliveriesByDeliverymanController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { FindDeliveriesByDeliverymanUseCase } from "./FindDeliveriesByDeliverymanUseCase";
+
+class FindDeliveriesByDeliverymanController {
+    async handle(request: Request, response: Response) {
+        const { id_deliveryman } = request;
+
+        const findDeliveriesByDeliverymanUseCase =
+            new FindDeliveriesByDeliverymanUseCase();
+
+        const deliveries =
+            findDeliveriesByDeliverymanUseCase.execute(id_deliveryman);
+
+        return response.json({ deliveries });
+    }
+}
+
+export { FindDeliveriesByDeliverymanController };
