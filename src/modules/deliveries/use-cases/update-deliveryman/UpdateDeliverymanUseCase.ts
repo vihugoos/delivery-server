4d5b6078de52ea1eb0ca6bffd63da02964bd3504
Victor Hugo <victorhugoos@live.com>
2023-01-18 18:52:03 -0300

    feat: UpdateDeliverymanUseCase.ts created

---
+++ src/modules/deliveries/use-cases/update-deliveryman/UpdateDeliverymanUseCase.ts
@@ -0,0 +1,23 @@
+import { prisma } from "../../../../database/prisma-client";
+
+interface IUpdateDeliveryman {
+    id_delivery: string;
+    id_deliveryman: string;
+}
+
+class UpdateDeliverymanUseCase {
+    async execute({ id_delivery, id_deliveryman }: IUpdateDeliveryman) {
+        const delivery = await prisma.deliveries.update({
+            where: {
+                id: id_delivery,
+            },
+            data: {
+                id_deliveryman,
+            },
+        });
+
+        return delivery;
+    }
+}
+
+export { UpdateDeliverymanUseCase };
