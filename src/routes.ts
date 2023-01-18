b304e0e4562c2fdb2c8ec2cbb9cfa0e5036e70f4
Victor Hugo <victorhugoos@live.com>
2023-01-18 01:03:20 -0300

    feat: added '/deliveryman' route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/use-cases/create-client/CreateClientController";
 import { AuthenticateClientController } from "./modules/accounts/use-cases/authenticate-client/AuthenticateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/use-cases/create-deliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 routes.post("/client", new CreateClientController().handle);
 
 routes.post("/authenticate", new AuthenticateClientController().handle);
+
+routes.post("/deliveryman", new CreateDeliverymanController().handle);
 
 export { routes };
