5bf9a0b7b7380315b8542c696493eb0e1de79687
Victor Hugo <victorhugoos@live.com>
2023-01-18 01:02:04 -0300

    feat: CreateDeliverymanController.ts created

---
+++ src/modules/deliveryman/use-cases/create-deliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateDeliverymanUseCase } from "./CreateDeliverymanUseCase";
+
+class CreateDeliverymanController {
+    async handle(request: Request, response: Response) {
+        const { username, password } = request.body;
+
+        const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+
+        const deliveryman = await createDeliverymanUseCase.execute({
+            username,
+            password,
+        });
+
+        return response.json(deliveryman);
+    }
+}
+
+export { CreateDeliverymanController };
