9948ee8dcd61f9f38971a682587a2e3724f1a782
Victor Hugo <victorhugoos@live.com>
2023-01-18 18:52:23 -0300

    feat: UpdateDeliverymanController.ts created

---
+++ src/modules/deliveries/use-cases/update-deliveryman/UpdateDeliverymanController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { UpdateDeliverymanUseCase } from "./UpdateDeliverymanUseCase";
+
+class UpdateDeliverymanController {
+    async handle(request: Request, response: Response) {
+        const { id: id_delivery } = request.params;
+        const { id_deliveryman } = request;
+
+        const updateDeliverymanUseCase = new UpdateDeliverymanUseCase();
+
+        const delivery = await updateDeliverymanUseCase.execute({
+            id_delivery,
+            id_deliveryman,
+        });
+
+        return response.json({ delivery });
+    }
+}
+
+export { UpdateDeliverymanController };
