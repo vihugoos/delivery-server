bf5588bee63da6bdffdb475d39dbf449d4f2b9f7
Victor Hugo <victorhugoos@live.com>
2023-01-18 01:38:08 -0300

    feat(routes.ts): added '/deliveryman/authenticate' route and another authenticate route renamed to '/client/authenticate'

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,23 @@ import { Router } from "express";
 import { CreateClientController } from "./modules/clients/use-cases/create-client/CreateClientController";
 import { AuthenticateClientController } from "./modules/accounts/use-cases/authenticate-client/AuthenticateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/use-cases/create-deliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "./modules/accounts/use-cases/authenticate-deliveryman/AuthenticateDeliveryController";
 
 const routes = Router();
 
+// Create a new client
 routes.post("/client", new CreateClientController().handle);
 
-routes.post("/authenticate", new AuthenticateClientController().handle);
+// Authenticate client in app
+routes.post("/client/authenticate", new AuthenticateClientController().handle);
 
+// Create a new delivery
 routes.post("/deliveryman", new CreateDeliverymanController().handle);
+
+// Authenticate deliveryman in app
+routes.post(
+    "/deliveryman/authenticate",
+    new AuthenticateDeliverymanController().handle
+);
 
 export { routes };
