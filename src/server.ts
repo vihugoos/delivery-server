96ad43801c965f159be5821d83bf8e6fd3ae071b
Victor Hugo <victorhugoos@live.com>
2023-01-18 00:36:44 -0300

    feat: implemented express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import { routes } from "./routes";
 
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof Error) {
+            return response.status(400).json({
+                message: err.message,
+            });
+        }
+
+        console.log("\nInternal Server Error (500):", err);
+
+        return response.status(500).json({
+            status: "Error",
+            message: `Internal Server Error (500) - ${err}`,
+        });
+    }
+);
 
 app.listen(3000, () =>
     console.log("\nHTTP Server is running on http://localhost:3000")
