07dbf27af5d0b129c880a594e30cc818b71a4e81
Victor Hugo <victorhugoos@live.com>
2023-01-18 02:32:19 -0300

    feat: CreateDeliveryUseCase.ts created

---
+++ src/modules/deliveries/use-cases/create-delivery/CreateDeliveryUseCase.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../../../../database/prisma-client";
+
+interface ICreateDelivery {
+    id_client: string;
+    item_name: string;
+}
+
+class CreateDeliveryUseCase {
+    async execute({ id_client, item_name }: ICreateDelivery) {
+        const delivery = await prisma.deliveries.create({
+            data: {
+                id_client,
+                item_name,
+            },
+        });
+
+        return delivery;
+    }
+}
+
+export { CreateDeliveryUseCase };
