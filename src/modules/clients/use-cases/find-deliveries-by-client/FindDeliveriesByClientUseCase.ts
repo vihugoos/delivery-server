5568bdacded728a077ee3ddb2f6c79872b0efddd
Victor Hugo <victorhugoos@live.com>
2023-01-18 19:31:11 -0300

    feat: FindDeliveriesByClientUseCase.ts created

---
+++ src/modules/clients/use-cases/find-deliveries-by-client/FindDeliveriesByClientUseCase.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../database/prisma-client";
+
+class FindDeliveriesByClientUseCase {
+    async execute(id_client: string) {
+        const deliveries = await prisma.clients.findMany({
+            where: {
+                id: id_client,
+            },
+            select: {
+                id: true,
+                username: true,
+                deliveries: true,
+            },
+        });
+
+        return deliveries;
+    }
+}
+
+export { FindDeliveriesByClientUseCase };
